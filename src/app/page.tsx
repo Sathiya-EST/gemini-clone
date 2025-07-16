d6ea2487196305319963b442df78e9f218f13e8c
sathiya <sathiya.est@gmail.com>
2025-07-16 10:15:29 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <p className="text-2xl border p-4">Hello World</p>
 
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      {/* <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -99,7 +99,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
